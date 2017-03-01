44699e36897de231aae7b2f24dc4a74a6f3002d5
Geraldine Morales <geraldine.morales02@gmail.com>
2017-02-28 21:54:58 -0500

    added else if

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,11 @@ var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else if (cardOne === cardThree) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
