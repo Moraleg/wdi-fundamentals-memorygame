5690aa7306f35d2dd8b6e356ab9a82d9b78cc415
Geraldine Morales <geraldine.morales02@gmail.com>
2017-03-02 14:21:27 -0500

    created cards with js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,27 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
+//console.log("JS file is connected to HTML! Woo!")
+//var cardOne = "king";
+//var cardTwo = "queen";
+//var cardFour = "queen";
 
-if (cardTwo === cardFour) {
-	alert("You found a match!");
-} else if (cardOne === cardThree) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}
+//if (cardTwo === cardFour) {
+//	alert("You found a match!");
+//} else if (cardOne === cardThree) {
+//	alert("You found a match!");
+//} else {
+	//alert("Sorry, try again.");
+//}
+
+var cards = ['queen', 'king', 'queen', 'king'];
+
+var board = document.getElementById('game-board');
+function createCards() {
+for (var i=0; i < cards.length; i++) {
+var cardSeries = document.createElement('div');
+	cardSeries.className = 'card';
+	board.appendChild(cardSeries);
+	
+ }
+
+} 
+
+createCards();
